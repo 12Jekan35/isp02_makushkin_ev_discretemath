2e717b05b1a0a31e8fa3f5d00110c293fdc6904a
Jeja <makushkinev34@gmail.com>
2020-02-20 17:42:57 +0400

    Methods array sorting

---
+++ self_task/work_07_02_2020/projects/Sort_MakushkinISP-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sort_MakushkinISP_2
+{
+    class Program
+    {
+        static void DisplayArray(int[] array)
+        {
+            foreach (int item in array)
+                Console.Write(item);
+            Console.WriteLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+
+            Sort.BubbleSort(array);
+
+            int[] array2 = { 1, 2, 3, 4, 5, 6 };
+
+            Sort.SelectionSort(array2);
+
+            Console.WriteLine("Bubble sort:");
+            DisplayArray(array);
+            Console.WriteLine();
+
+            Console.WriteLine("Selected sort:");
+            DisplayArray(array2);
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ self_task/work_07_02_2020/projects/Sort_MakushkinISP-2/Sort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Sort_MakushkinISP_2
+{
+    static class Sort
+    {
+        public static void Swap(ref int firstItem, ref int secondItem)
+        {
+            int temp = firstItem;
+            firstItem = secondItem;
+            secondItem = temp;
+        }
+        public static void BubbleSort(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+
+            bool swapped = false;
+            while (true)
+            {
+                swapped = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int maxIndex = i;
+                for (int next = i + 1; next < array.Length; next++)
+                    if (array[maxIndex] < array[next])
+                        maxIndex = next;
+
+                if (maxIndex != i)
+                    Swap(ref array[i], ref array[maxIndex]);
+                
+            }
+        }
+    }
+}
